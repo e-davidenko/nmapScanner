8fc3c7a4d52dce41f60f69dc6ed1b44f7353c740
Excecc <e.davidenko@live.ru>
2022-09-20 00:22:48 +0300

    new

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ import nmap3
 scan = nmap3.Nmap()
 version_result = scan.nmap_version_detection("192.168.0.1")
 for i in version_result["192.168.0.1"]["ports"]:
-    print(i["portid"], i["state"], i["service"]["name"], i["service"].get('version', '-'))
+    print("Номер порта:", i["portid"], ", состояние порта:", i["state"], ", имя службы:", i["service"]["name"], ", версия:", i["service"].get('version', '-'))
